f08326dc22628a288785b8beaf0245971954d485
Gavibaik123 <aldzukhwan@gmail.com>
2025-10-05 20:14:39 +0700

    kelar pakk

---
+++ index.js
@@ -0,0 +1,31 @@
+a = 4
+b = 5
+const perkalian = () => {
+console.log(`jika ${a} dikali ${b} maka hasilnya ${a * b}`);
+}
+perkalian()
+
+const penjumlahan = () => {
+console.log(`jika ${a} ditambah ${b} maka hasilnya ${a + b}`);
+}
+penjumlahan()
+
+const pengurangan = () => {
+console.log(`jika ${a} dikali ${b} maka hasilnya ${a - b}`);
+}
+pengurangan()
+
+const pembagian = () => {
+console.log(`jika ${a} dibagi ${b} maka hasilnya ${a / b}`);
+}
+pembagian()
+
+const pangkat = () => {
+console.log(`jika ${a} dipangkatkan ${b} maka hasilnya ${a ** b}`);
+}
+pangkat()
+
+const modulus = () => {
+console.log(`jika ${a} modulus ${b} maka hasilnya ${a % b}`);
+}
+modulus()
